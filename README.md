a0eb86d6f71385752d6973fe1f7f6273bce863a9
Navneet Jain <navneetjain89@gmail.com>
2024-04-22 18:23:26 +0530

    test

---
+++ README.md
@@ -0,0 +1,5 @@
+# Description #
+This is a manufacturing execution system application created by IndusWorks. This app is intended to be run on Raspberry Pi. 
+
+# Steps #
+Download and install /dist/indusmes-0.0.3-py3-none-any.whl
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+def read_requirements():
+    with open('requirements.txt', 'r') as req:
+        content = req.read()
+        requirements = [i for i in content.split('\n') if i and '===' not in i]
+
+    return requirements
+
+setup(
+    name= "indusmes",
+    version = "0.0.3",
+    description= "MES Application from IndusWorks",
+    author = "Navneet Jain",
+    author_email="navneet@indusworks.in",
+    packages = find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires = read_requirements(),
+    entry_points={
+        'gui_scripts': [
+            'indusmes=indusmes.main:main',
+        ],
+    },
+)
